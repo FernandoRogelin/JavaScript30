e46a4040caaa8030df486483b2c127a040500dbd
FernandoRogelin <nando_rogelin@hotmail.com>
2017-12-27 21:35:20 -0200

    [trocado] nome de funções

--- Drum Kit/js/drumkit.js
+++ Drum Kit/js/drumkit.js
@@ -1,18 +1,17 @@
-function removeTransition(e) {
-  if (e.propertyName !== 'transform') return;
-  e.target.classList.remove('playing');
+function removeEfeito(event) {
+  if (event.propertyName !== 'transform') return;
+  event.target.classList.remove('efeito');
 }
 
-function playSound(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-  console.log(key);
+function tocaSom(event) {
+  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${event.keyCode}"]`);
   if (!audio) return;
-  key.classList.add('playing');
+  key.classList.add('efeito');
   audio.currentTime = 0;
   audio.play();
 }
 
 const keys = Array.from(document.querySelectorAll('.key'));
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-window.addEventListener('keydown', playSound);
+keys.forEach(key => key.addEventListener('transitionend', removeEfeito));
+window.addEventListener('keydown', tocaSom);
