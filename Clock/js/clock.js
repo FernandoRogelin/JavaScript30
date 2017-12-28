8de1cf6cb011d8f14d386920fc496fb2df0ee4a5
fernando <nando_rogelin@hotmail.com>
2017-12-28 14:51:58 -0200

    [arrumado] ponteiro dos relógios

--- Clock/js/clock.js
+++ Clock/js/clock.js
@@ -2,7 +2,7 @@ const segundosPonteiro = document.querySelector('.segundos');
 const minutosPonteiro = document.querySelector('.minutos');
 const horasPonteiro = document.querySelector('.horas');
 
-function trocaData(){
+function relogio(){
     const agora = new Date();
 
     const segundos = agora.getSeconds();
@@ -10,12 +10,12 @@ function trocaData(){
     segundosPonteiro.style.transform = `rotate(${segundosDegrees}deg)`;
 
     const minutos = agora.getMinutes();
-    const minutosDegrees = ((minutos / 60) * 360) + 90;
+    const minutosDegrees = ((minutos / 60) * 360) + ((segundos / 60) * 6) + 90;
     minutosPonteiro.style.transform = `rotate(${minutosDegrees}deg)`;
 
-    const horas = agora.getMinutes();
-    const horasDegrees = ((horas / 12) * 360) + 90;
+    const horas = agora.getHours();
+    const horasDegrees = ((horas / 12) * 360) + ((minutos / 60) * 30) + 90;
     horasPonteiro.style.transform = `rotate(${horasDegrees}deg)`;
 }
 
-setInterval(trocaData, 1000);
+setInterval(relogio, 1000);
